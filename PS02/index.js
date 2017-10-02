0e0ed57099e7675ca0ef4cf09fd0bffb565ac531
xunchongneu <31869452+xunchongneu@users.noreply.github.com>
2017-10-02 10:11:33 -0400

    Add files via upload

--- PS02/index.js
+++ PS02/index.js
@@ -1,3 +1,47 @@
 var svg = d3.select('svg');
 
 /* Your code goes here */
+
+
+d3.csv('./data.csv', function(dataIn){
+    console.log(dataIn);
+
+    svg.selectAll('circle')
+        .data(dataIn)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d){
+            return d.x;
+
+        })
+        .attr('cy', function(d){
+            return d.y;
+
+        })
+
+        .attr('r', function(d){
+            return d.r;
+
+        })
+        .attr('fill', function(d){
+            return d.fill;
+
+        })
+
+
+       ;
+
+
+
+
+
+})
+
+
+var scaleRuler = d3.scaleLinear().domain([0,100]).range([0,10]);
+
+console.log(scaleRuler(95))
+
+var scaleSizes = d3.scaleOrdinal().domain(["small","medium","large"]).range([30,50,90]);
+
+console.log(scaleSizes("large"))
